e7da0fbe37344fcdd1284995e731312cf70a2a3c
SantiCastelli <castellist@gmail.com>
2022-03-08 09:42:56 +0100

    terminado

--- ex1.js
+++ ex1.js
@@ -1,8 +1,9 @@
 // Implementa una función, que dado un número de minutos, devuelva los segundos a los que corresponden dichos minutos
 
 function minutosASegundos(minutos) {
-
+    return  minutos *60
 }
 
 console.log(minutosASegundos(3)); // 180
 console.log(minutosASegundos(5)); // 300
+
--- ex5.js
+++ ex5.js
@@ -1,6 +1,13 @@
 // Implementa una función que recorre toda la 'palabra', y cuenta todas las letras 'letra' que contiene. Deve devolver el número de veces que aparece 'letra' en 'palabra'
 
 function contarLetras(palabra, letra) {
+    let numVeces = 0
+    for (let i = 0; i < palabra.length; i++) {
+        if (palabra[i] == letra){
+            numVeces++;
+        }     
+    }
+    return numVeces
 }
 
 console.log(contarLetras("pato", "p")); // 1
